688b5cc09df14a090889d49476bed04402daa435
KingPhilip14 <kingian404@gmail.com>
2026-03-31 11:31:33 -0500

    Added the structure for a test suite

---
+++ test_suite/runner.py
@@ -0,0 +1,19 @@
+import unittest
+"""
+This file is used to run the test suite. 
+"""
+
+if __name__ == "__main__":
+    loader = unittest.TestLoader()
+    tests = loader.discover('.')
+    testRunner = unittest.TextTestRunner()
+    test_results = testRunner.run(tests)
+
+    if test_results.wasSuccessful():
+        exit(0)
+    else:
+        exit(1)
+
+# To run the test suite, make sure your terminal is in the root directory of the project
+# Then, in your terminal, run 'python -m test_suite.runner'.
+# This command runs this file as a module of the entire project, allowing imports to function properly
