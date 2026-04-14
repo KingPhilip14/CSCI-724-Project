78ea9359831e81932e74a4e9cc7b6717199bb174
Ian King <114031398+KingPhilip14@users.noreply.github.com>
2026-04-14 10:23:25 -0500

    Updated the serialize class to make reading data from JSON files easier (#6)
    * Updated the serialize class to make reading data from JSON files easier
    
    * Changed 'mem_space' to 'peak_mem' for serialization
    
    * Changed peak_mem to be ints instead of bytes

--- serialize.py
+++ serialize.py
@@ -9,18 +9,18 @@ import base64
 
 
 class Serialize:
-    def __init__(self, curr_mode: SimMode = SimMode.HUMAN):
+    def __init__(self, curr_mode: SimMode = SimMode.HUMAN, trial_num: int = 1):
         self.curr_mode = curr_mode
         self.curr_mode_name: str = curr_mode.name.lower()
-        self.trial_num = 1
+        self.trial_num = trial_num
         self.data_file_path: str = get_data_file_path(self.curr_mode_name, self.trial_num)
 
-    def serialize(self, score: int, turns: int, mem_space: bytes, exec_time: float) -> None:
+    def serialize(self, score: int, turns: int, peak_mem: int, exec_time: float) -> None:
         """
         Collects all major data points needed for metric analysis and stores them as JSON files in the data directory.
         :param score: The total score gained for the current execution run.
         :param turns: The total turns it took to complete the current execution run.
-        :param mem_space: The total memory space used by the used algorithm.
+        :param peak_mem: The total memory space used by the used algorithm.
         :param exec_time: The total time it took to complete the current execution run.
         :return: None
         """
@@ -30,7 +30,7 @@ class Serialize:
         data: dict = {
             'score': score,
             'turns': turns,
-            'mem_space': base64.b64encode(mem_space).decode('utf-8'),
+            'peak_mem': peak_mem,
             'exec_time': exec_time,
         }
 
@@ -40,13 +40,14 @@ class Serialize:
             json.dump(data, f)
             f.close()
 
-    def get_json_data(self) -> dict[str, Any]:
+    def get_json_data(self, path: str | None = None) -> dict[str, Any]:
         """
-        Reads the data file and returns a dictionary with all the data points collected. Raises an error if the
-        file does not exist.
+        Takes in an optional parameter of a data file path. If the given path exists, it will be used to extract the
+        data from the JSON file. If no file path is given, the default using the current settings of object variables
+        will be used. If an invalid path is given, or if it doesn't exist, an error will be thrown.
         :return: A dict with all the data points
         """
-        path: str = get_data_file_path(self.curr_mode_name, self.trial_num)
+        path: str = get_data_file_path(self.curr_mode_name, self.trial_num) if path is None or path == '' else path
 
         if not os.path.exists(path):
             raise FileNotFoundError(f'File in path "{path}" does not exist')
@@ -57,6 +58,6 @@ class Serialize:
         return {
             'score': data['score'],
             'turns': data['turns'],
-            'mem_space': base64.b64decode(data['mem_space']),
+            'peak_mem': data['peak_mem'],
             'exec_time': data['exec_time'],
         }
--- test_suite/tests/test_serialize.py
+++ test_suite/tests/test_serialize.py
@@ -20,7 +20,7 @@ class TestSerialize(unittest.TestCase):
         self.turns: int = 5
 
         # a fake value for testing bytes; no actual significance or relation to the expected mem size
-        self.mem_space: bytes = bytes('a', 'utf-8')
+        self.peak_mem: int = 123456789
 
         self.exec_time: float = 10.0
 
@@ -30,7 +30,7 @@ class TestSerialize(unittest.TestCase):
         get_json_data function.
         :return: None
         """
-        self.serialize.serialize(self.score, self.turns, self.mem_space, self.exec_time)
+        self.serialize.serialize(self.score, self.turns, self.peak_mem, self.exec_time)
 
         data_path: str = utils.get_data_file_path(self.serialize.curr_mode_name, self.serialize.trial_num)
 
@@ -38,12 +38,44 @@ class TestSerialize(unittest.TestCase):
         self.assertTrue(os.path.exists(data_path))
 
         # read the data from the file and test if it matches what was written
+        # already tests if not given a specific path
         data = self.serialize.get_json_data()
 
         self.assertEqual(data['score'], self.score)
         self.assertEqual(data['turns'], self.turns)
-        self.assertEqual(data['mem_space'], self.mem_space)
+        self.assertEqual(data['peak_mem'], self.peak_mem)
         self.assertEqual(data['exec_time'], self.exec_time)
+
+    def test_given_new_path(self) -> None:
+        """
+        Creates `human_trial_2.json` and `human_trial_3.json`. Then, the specified path to `human_trial_3.json`
+        is used to extract data.
+        :return:
+        """
+        # create human_trial_2.json
+        self.serialize = Serialize(trial_num=2)
+        self.serialize.serialize(self.score + 10, self.turns + 10, self.peak_mem, self.exec_time + 50)
+        data_path: str = utils.get_data_file_path(self.serialize.curr_mode_name, self.serialize.trial_num)
+
+        # test if the json file was created
+        self.assertTrue(os.path.exists(data_path))
+
+        # create human_trial_3.json
+        self.serialize = Serialize(trial_num=3)
+        self.serialize.serialize(self.score + 17, self.turns + 5, self.peak_mem, self.exec_time + 30)
+        data_path = utils.get_data_file_path(self.serialize.curr_mode_name, self.serialize.trial_num)
+
+        # test if the json file was created
+        self.assertTrue(os.path.exists(data_path))
+
+        # read the data from the file and test if it matches what was written
+        # already tests if not given a specific path
+        data = self.serialize.get_json_data(data_path)
+
+        self.assertEqual(data['score'], self.score + 17)
+        self.assertEqual(data['turns'], self.turns + 5)
+        self.assertEqual(data['peak_mem'], self.peak_mem)
+        self.assertEqual(data['exec_time'], self.exec_time + 30)
 
 
     def test_get_json_data_raised_error(self) -> None:
